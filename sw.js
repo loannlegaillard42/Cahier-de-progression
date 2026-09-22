371bb0d75d135a519979f8edc1c86e74c66b2e50
loannlegaillard42 <loann.legaillard42@gmail.com>
2026-09-22 17:40:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cahier-hg-v8";
+const CACHE = "cahier-hg-v9";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
